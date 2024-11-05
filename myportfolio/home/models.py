61ab5fecde0d743ae2df66d1fbf9429c800f93ba
eva <vineethm430@gmail.com>
2024-11-05 22:43:00 +0530

    bats fly effect finished

--- myportfolio/home/models.py
+++ myportfolio/home/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 
 class Portfolio(models.Model):
-    title =models.CharField(max_length=50)
-    description=models.TextField(max_length=350)
-    image=models.ImageField(upload_to='images/',blank=True)
-    url=models.URLField(blank=True)
-    video=models.FileField(upload_to='videos/',blank=True)
-    date=models.DateField(auto_now_add=True)
+    title =models.CharField(max_length=50,null=False)
+    description=models.TextField(max_length=350,null=False)
+    url=models.URLField(blank=True,null=True)
+    image=models.ImageField(upload_to='images/',blank=True,null=True)
+    video=models.FileField(upload_to='videos/',blank=True,null=True)
+    date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
