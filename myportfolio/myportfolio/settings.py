669a6e45e515bff83d5301d725abff654a7ab313
eva <vineethm430@gmail.com>
2024-11-11 22:20:03 +0530

    commit before deployment to render

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zw529^ujhcz*rcbhq*y$!y-64+04kg4$s5d%miob(p_s#(%-7-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -128,7 +128,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR/"static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
@@ -152,7 +151,5 @@ EMAIL_HOST_PASSWORD = 'hlzltxjudylvssbw'
 
 CONTACT_EMAIL = 'mvineeth443@gmail.com'
 
- #Activate Django Heroku.
-django_heroku.settings(locals())
-
+ 
 
